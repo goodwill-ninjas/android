b7cc35df1b661d255575aaaced8a6d6cf9d3f2c7
wojciech mierzejewski <55141520+mierzvoj@users.noreply.github.com>
2023-03-06 21:38:39 +0100

    Add WIP Calendar picker for testing by AJ

--- app/src/main/java/pl/edu/pjatk/goodwill_ninjas/blooddonor_android/addDonation/AddDonationFirstScreen.kt
+++ app/src/main/java/pl/edu/pjatk/goodwill_ninjas/blooddonor_android/addDonation/AddDonationFirstScreen.kt
@@ -5,8 +5,7 @@ import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-
-
+import androidx.activity.compose.setContent
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.*
@@ -14,23 +13,34 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.maxkeppeker.sheets.core.models.base.rememberSheetState
 import com.maxkeppeler.sheets.calendar.CalendarDialog
 import com.maxkeppeler.sheets.calendar.models.CalendarConfig
 import com.maxkeppeler.sheets.calendar.models.CalendarSelection
 import com.maxkeppeler.sheets.calendar.models.CalendarStyle
-
+import pl.edu.pjatk.goodwill_ninjas.blooddonor_android.util.getDefaultDateInMillis
 import java.text.SimpleDateFormat
+
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.*
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DatePicker() {
+fun DatePicker(
+
+
+) {
+    val dateSelected = remember { mutableStateOf("") }
+    val simpleDateFormatPattern = "EEE, MMM d"
+    val dateFormat = SimpleDateFormat(simpleDateFormatPattern, Locale.getDefault())
+    dateFormat.timeZone = TimeZone.getTimeZone("UTC")
+
+
     val calendarState = rememberSheetState()
 
     val formattedDate by remember {
@@ -38,11 +48,13 @@ fun DatePicker() {
             DateTimeFormatter.ofPattern("MMM dd yyyy").format(LocalDate.now())
         }
     }
+    val onSelectDate = ""
     CalendarDialog(state = calendarState,
         config = CalendarConfig(
-            style = CalendarStyle.WEEK
+
         ),
-        selection = CalendarSelection.Date { val onSelectDate: (date: LocalDate) -> Unit
+        selection = CalendarSelection.Date { _ ->
+            Log.d("selected date", "$dateSelected")
 
         })
     Column(
@@ -56,23 +68,34 @@ fun DatePicker() {
     ) {
         Button(onClick = { calendarState.show() }) {
             Text(text = "Wybierz datę: $formattedDate")
-
+            Log.d("selected date", "$formattedDate")
         }
-
 
     }
 
 }
 
+@Composable
+fun SimpleCard() {
+    val paddingModifier = Modifier.padding(10.dp)
+    Card(modifier = paddingModifier) {
+        Text(
+            text = "Simple Card with elevation",
+            modifier = paddingModifier
+        )
+    }
+    Column() {
+
+    }
+}
 
 @Preview()
 @Composable
 fun DatePickerPreview() {
 
     Surface {
-        DatePicker(
-
-        )
+        DatePicker()
+        SimpleCard()
     }
 
 }
