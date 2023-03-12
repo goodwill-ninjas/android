0a4a4d23b3c01f01d801d7e9b170854581ff5e13
wojciech mierzejewski <55141520+mierzvoj@users.noreply.github.com>
2023-03-12 21:24:02 +0100

    Add Bottom Sheet
    Add data fields in Add Donation welcome screen
    
    Add donation welcome screen
    
    Add blood qty
    
    Add blood qty
    
    Add bottom sheet

---
+++ app/src/main/java/pl/edu/pjatk/goodwill_ninjas/blooddonor_android/addDonation/DaysCalculator.kt
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.goodwill_ninjas.blooddonor_android.addDonation
+
+import java.time.LocalDate
+import java.time.Period
+
+fun DaysCalculator() {
+    var today = LocalDate.now()
+
+    //hardcoded theoretical date
+    var lastDonation = LocalDate.parse("2023-2-20")
+    var nextDonation = LocalDate.parse("2023-2-20").plusDays(57)
+    var periodDays = Period.between(nextDonation, today)
+}
