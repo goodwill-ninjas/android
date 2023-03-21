642bafa624afaf8805589ee63edc6b92ef85710a
matgrube <47569899+matgrube@users.noreply.github.com>
2023-03-21 19:51:30 +0100

    DEV-12 Added Main page and navigation tooling (#3)

---
+++ app/src/main/java/pl/edu/pjatk/goodwill_ninjas/blooddonor_android/components/BottomBar.kt
@@ -0,0 +1,69 @@
+package pl.edu.pjatk.goodwill_ninjas.blooddonor_android.components
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import pl.edu.pjatk.goodwill_ninjas.blooddonor_android.components.prepareBottomMenu
+
+@Composable
+fun MyBottomBar() {
+    val bottomMenuItemsList = prepareBottomMenu()
+
+    val contextForToast = LocalContext.current.applicationContext
+
+    var selectedItem by remember {
+        mutableStateOf("Home")
+    }
+
+    BottomAppBar(
+        cutoutShape = CircleShape
+    ) {
+        bottomMenuItemsList.forEachIndexed { index, _ ->
+            if (index == 1) {
+                BottomNavigationItem(
+                    selected = false,
+                    onClick = {},
+                    icon = {},
+                    enabled = false
+                )
+            }
+
+            Box(modifier = Modifier.fillMaxSize()) {
+                BottomNavigation(
+                    modifier = Modifier.align(alignment = Alignment.BottomCenter)
+                ) {
+
+                    bottomMenuItemsList.forEach { menuItem ->
+
+                        BottomNavigationItem(
+                            selected = (selectedItem == menuItem.label),
+                            onClick = {
+                                selectedItem = menuItem.label
+                                Toast.makeText(
+                                    contextForToast,
+                                    menuItem.label, Toast.LENGTH_SHORT
+                                ).show()
+                            },
+                            icon = {
+                                Icon(
+                                    imageVector = menuItem.icon,
+                                    contentDescription = menuItem.label
+                                )
+                            },
+                            label = {
+                                Text(text = menuItem.label)
+                            },
+                            enabled = true
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/pl/edu/pjatk/goodwill_ninjas/blooddonor_android/ui/theme/Color.kt
+++ app/src/main/java/pl/edu/pjatk/goodwill_ninjas/blooddonor_android/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Red200 = Color(0xFF876968)
+val Red500 = Color(0xFF901C11)
+val Red700 = Color(0xFF710508)
+val OffColor = Color(0xFF8CB4B0)
