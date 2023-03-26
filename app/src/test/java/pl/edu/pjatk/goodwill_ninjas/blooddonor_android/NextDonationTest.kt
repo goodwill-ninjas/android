0c5053614bd19e3b18695307099dea7dd83161f6
Mateusz Grube <m.grube@outlook.com>
2023-03-26 13:03:10 +0200

    DEV-79 added tests

---
+++ app/src/test/java/pl/edu/pjatk/goodwill_ninjas/blooddonor_android/NextDonationTest.kt
@@ -0,0 +1,20 @@
+package pl.edu.pjatk.goodwill_ninjas.blooddonor_android
+
+import org.junit.Test
+import org.junit.Assert.*
+import pl.edu.pjatk.goodwill_ninjas.blooddonor_android.components.nextDonation.NextDonation
+import pl.edu.pjatk.goodwill_ninjas.blooddonor_android.utils.DonationType
+import java.time.LocalDateTime
+
+class NextDonationTest {
+    private val nextDonation = NextDonation(LocalDateTime.now(), DonationType.FULL)
+    private val nextDonationLongTimeAgo = NextDonation(LocalDateTime.now().minusDays(365), DonationType.FULL)
+    @Test
+    fun testCalculateNextDonation() {
+        assertTrue(nextDonation.calculateNextDonation() > 84)
+    }
+    @Test
+    fun testCalculateNextDonationForLongTimeAgo() {
+        assertTrue(nextDonationLongTimeAgo.calculateNextDonation() < 0)
+    }
+}
