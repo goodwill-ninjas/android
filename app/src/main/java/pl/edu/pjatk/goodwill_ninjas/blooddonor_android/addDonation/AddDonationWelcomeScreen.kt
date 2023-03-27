225684e9069c098c5d39a19f3b26656706cdeed0
wojciech mierzejewski <55141520+mierzvoj@users.noreply.github.com>
2023-03-27 21:52:50 +0200

    Add donation calendar

--- app/src/main/java/pl/edu/pjatk/goodwill_ninjas/blooddonor_android/addDonation/AddDonationWelcomeScreen.kt
+++ app/src/main/java/pl/edu/pjatk/goodwill_ninjas/blooddonor_android/addDonation/AddDonationWelcomeScreen.kt
@@ -1,22 +1,29 @@
 package pl.edu.pjatk.goodwill_ninjas.blooddonor_android.addDonation
 
+import android.annotation.SuppressLint
+import android.util.Log
 import android.view.Surface
+import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import pl.edu.pjatk.goodwill_ninjas.blooddonor_android.navigation.Screen
 import java.text.DateFormat
 import java.time.LocalDate
 import java.util.Calendar
-
 
 
 @Composable
@@ -24,68 +31,43 @@ import java.util.Calendar
 fun WelcomeScreen() {
     val paddingModifier = Modifier.padding(10.dp)
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.width(300.dp),
         horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.Center
     ) {
         Surface(
-            modifier = Modifier
-                .width(200.dp)
-                .height(70.dp)
+
         ) {
             Card(
+                modifier = Modifier.fillMaxWidth(),
                 elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
-                modifier = paddingModifier
-            ) {
+
+                ) {
                 Text(
                     text = "Krew pełna", fontSize = 20.sp, fontWeight = FontWeight.Bold,
-                    modifier = paddingModifier
-                )
-            }
-        }
-        Surface(
-            modifier = Modifier
-                .width(200.dp)
-                .height(60.dp)
-        ) {
-            Card(
-                elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
-                modifier = paddingModifier
-            ) {
-                Column() {
-                    GetDate()
 
 
-                }
-            }
-        }
-
-        Surface(
-            modifier = Modifier
-                .width(300.dp)
-                .height(100.dp)
-        ) {
-            Card(
-                elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
-                modifier = paddingModifier
-            ) {
-                Column() {
-                    Text(
-                        text = "Możesz oddać krew pełną już za: ",
-
-                        modifier = paddingModifier
                     )
-                    Row {
-//                        GetDate()
-                        PeriodInDays()
-                    }
-
-
+                Column {
+                    GetDate()
                 }
+
             }
         }
-    }
 
+        Card(
+            elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+            modifier = paddingModifier
+        ) {
+
+            Text(text = "Dodaj donację", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            DateButton()
+//            BloodQtyInput()
+
+
+        }
+
+    }
 }
 
 
@@ -97,23 +79,89 @@ fun GetDate() {
 
 }
 
+//@SuppressLint("UnrememberedMutableState")
 @Composable
-fun PeriodInDays(){
-val daysRemaining = 50;
-    Text(text = "$daysRemaining")
-
-
+fun DateButton() {
+    val showDialog = remember { mutableStateOf(false) }
+    if (showDialog.value) {
+        alert(msg = "Hello, this is an alert dialog!",
+            showDialog = showDialog.value,
+            onDismiss = { showDialog.value = false })
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = {
+//                showDialog.value = true
+            }
+        ) {
+            Text(text = "Click Me")
+        }
+    }
 }
+
+@Composable
+fun alert(
+    msg: String,
+    showDialog: Boolean,
+    onDismiss: () -> Unit
+) {
+    if (showDialog) {
+        AlertDialog(
+            title = {
+                Text(msg)
+            },
+            onDismissRequest = onDismiss,
+            confirmButton = {
+                TextButton(onClick = onDismiss) {
+                    Text("Dismiss")
+                }
+            },
+            dismissButton = {}
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun BloodQtyInput() {
+
+    var value by remember {
+        mutableStateOf("false")
+    }
+
+    val context = LocalContext.current.applicationContext
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+//        verticalArrangement = Arrangement.Center
+    ) {
+
+        TextField(
+            value = value,
+            onValueChange = { newText ->
+                value = newText
+            },
+            label = { Text(text = "Ilość") },
+            placeholder = { Text(text = "Ilość") },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    Toast.makeText(context, "On Search Click: value = $value", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            )
+        )
+    }
+}
+
 
 @Preview()
 @Composable
 fun WelcomeScreenPreview() {
-
     Surface {
-
         WelcomeScreen()
-
-
     }
-
 }
